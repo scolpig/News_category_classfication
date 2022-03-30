7ec509d55d780c48920670117b0a319f03c04a2d
kang-narae <raenakang111@gmail.com>
2022-03-30 16:51:23 +0900

    크롤링 취합했음. 파일명은 naver_news_sum.csv

---
+++ crawling_data/concat.py
@@ -0,0 +1,21 @@
+
+import pandas as pd
+
+Politics= pd.read_csv('./naver_news_politics.csv', sep=',' , index_col=0)
+Economic= pd.read_csv('./naver_news_Economic.csv', sep=',', index_col=0)
+Social= pd.read_csv('./naver_news_Social.csv', sep=',' , index_col=0)
+Culture= pd.read_csv('./naver_news_Culture_20220330.csv', sep=',', index_col=0)
+IT= pd.read_csv('./naver_news_IT_20220330.csv', sep=',', index_col=0)
+World= pd.read_csv('./naver_news_World_20220330.csv', sep=',', index_col=0)
+
+
+
+df_sum = pd.DataFrame()
+
+df_sum= pd.concat([Politics, Economic, Social, Culture, IT, World ], axis='rows' , ignore_index=True)
+# df_sum.reset_index(drop=True)
+
+print(df_sum.head())
+print(df_sum.info())
+
+df_sum.to_csv('./naver_news_sum.csv')
--- job02_crawling.py
+++ job02_crawling.py
@@ -1,15 +1,24 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 import pandas as pd
 import re
 import time
+
+def crawl_title():
+    try:
+
+        title = driver.find_element_by_xpath('//*[@id="section_body"]/ul[{}]/li[{}]/dl/dt[2]/a'.format(j, i)).text
+        title = re.compile('[^가-힣a-zA-Z ]').sub(' ', title)
+        title_list.append(title)
+    except NoSuchElementException:
+        print('NoSuchElementException')
 
 
 option = webdriver.ChromeOptions()
 # options.add_argument('headless')  이거 활성화하면 웹 브라우저가 안 뜸. 보고싶으면 주석 풀면 되고. 근데지금은 주석하래. 지금은 이거 하면 에러뜬대
 
 option.add_argument('lang=ko_KR')
-option.add_argument('--no-sandbox')
+option.add_argument('--no-sandbox')   #이 아래 3개는 맥 어쩌고에서 필요한 거임. 윈도우 주석 풀어놔도 괜찮음.
 option.add_argument('--disable-dev-shm-usage')
 option.add_argument('disable-gpu')
 
@@ -20,7 +29,7 @@ driver.implicitly_wait(10)
 category = ['Politics', 'Economic', 'Social',
             'Culture',  'IT', 'World']
 
-page_num = [242, 374, 486, 71, 76, 125]
+page_num = [140, 374, 486, 71, 76, 125]
 
 
 #https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=100#&date=%2000:00:00&page=1
@@ -30,22 +39,29 @@ page_num = [242, 374, 486, 71, 76, 125]
 
 df_title = pd.DataFrame()
 
-for l in range(6):
-    # for k in range(1, page_num[l]+1):
-    for k in range(1, 3):
+for l in range(0,1):
+    title_list = []
+    for k in range(1, page_num[l]+1):
+    # for k in range(1, 3):
         url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=10{}#&date=%2000:00:00&page={}'.format(l, k)
-        title_list = []
-        # for j in range(1, 5):
-        for j in range(1, 3):
-            # for i in range(1, 6):
-            for i in range(1, 3):
+
+        driver.get(url)
+        time.sleep(0.5)
+        for j in range(1, 5):
+        # for j in range(1, 3):
+            for i in range(1, 6):
+            # for i in range(1, 3):
                 try:
+                    crawl_title()
+                except StaleElementReferenceException:
+                    # print('StaleElementReferenceException')
                     driver.get(url)
-                    title = driver.find_element_by_xpath('//*[@id="section_body"]/ul[{}]/li[{}]/dl/dt[2]/a'.format(j, i)).text
-                    title = re.compile('[^가-힣a-zA-Z ]').sub(' ', title)
-                    title_list.append(title)
-                except NoSuchElementException:
-                    print('NoSuchElementException')
+                    time.sleep(0.5)
+                    crawl_title()
+                except:
+                    print('error')
+
+
     df_section_title = pd.DataFrame(title_list, columns= ['title'])
     df_section_title['category'] = category[l]
     df_title= pd.concat([df_title, df_section_title], axis='rows', ignore_index=True)
@@ -53,5 +69,5 @@ for l in range(6):
 driver.close()
 df_title.info()
 print(df_title.category.value_counts())
-df_title.to_csv('./crawling_data/naver_news_title_20220330.csv')
+df_title.to_csv('./crawling_data/naver_news_politics.csv')
 
