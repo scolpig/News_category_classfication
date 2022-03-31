fe06a3a994460d6faeb777011dba4af6c96314a7
scolpig <scolpig@gmail.com>
2022-03-31 11:38:08 +0900

    preprocessing final

--- job04_preprocessing.py
+++ job04_preprocessing.py
@@ -29,11 +29,46 @@ okt = Okt()
 for i in range(len(X)):
     X[i] = okt.morphs(X[i], stem=True)
 
-stopwords =
-
+stopwords = pd.read_csv('./crawling_data/stopwords.csv',
+                        index_col=0)
+for j in range(len(X)):
+    words = []
+    for i in range(len(X[j])):
+        if len(X[j][i]) > 1:
+            if X[j][i] not in list(stopwords['stopword']):
+                words.append(X[j][i])
+    X[j] = ' '.join(words)
 print(X[1])
 
+token = Tokenizer()
+token.fit_on_texts(X)
+tokened_X = token.texts_to_sequences(X)
+print(tokened_X[0])
+with open('./output/news_token.pickle', 'wb') as f:
+    pickle.dump(token, f)
 
+wordsize = len(token.word_index) + 1
+print(wordsize)
+print(token.word_index)
+
+max = 0
+for i in range(len(tokened_X)):
+    if max < len(tokened_X[i]):
+        max = len(tokened_X[i])
+print(max)
+
+X_pad = pad_sequences(tokened_X, max)
+print(X_pad[:10])
+
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X_pad, onehot_Y, test_size=0.1)
+print(X_train.shape, Y_train.shape)
+print(X_test.shape, Y_test.shape)
+
+xy = X_train, X_test, Y_train, Y_test
+np.save(
+    './crawling_data/news_data_max_{}_wordsize_{}'.format(
+        max, wordsize), xy)
 
 
 
