91aba31fa740ec8eb879dcf9715a7d3450b01d5d
Kimsuchan <tncksrla96@gmail.com>
2022-03-31 17:16:29 +0900

    requirements

--- job03_concat.py
+++ job03_concat.py
@@ -3,10 +3,10 @@ import glob
 data_paths = glob.glob('./crawling_data/*')
 # print(data_paths)
 df1 = pd.DataFrame()
-for path in data_paths[3:8]:
+for path in data_paths[4:9]:
     df_temp = pd.read_csv(path, index_col=0)
     df1 = pd.concat([df1, df_temp], ignore_index=True, axis='rows')
-for path in data_paths[-1:]:
+for path in data_paths[10:11]:
     df_temp = pd.read_csv(path, index_col=0)
     pd.concat([df1, df_temp], ignore_index=True, axis='rows')
 # pd.concat([df1, df2], ignore_index=True, axis='rows')
